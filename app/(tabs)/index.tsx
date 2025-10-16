f480a121c370ebb61c51c8c424a51ebe2185d927
Gerson Fujimoto Gordiano <118776690+Fujimoto294@users.noreply.github.com>
2025-10-16 15:24:11 +0000

    Alteracao formulario

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,10 @@
 import React, { useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import perguntas from './perguntas';
+
 
 export default function App() {
-  const perguntas = [
-    { texto: "Seu parceiro ou ex-parceiro já impediu você de trabalhar ou de ter acesso ao seu próprio dinheiro?", peso: 2 },
-    { texto: "Ele já destruiu, escondeu, vendeu ou se apropriou dos seus bens, documentos ou dinheiro sem sua permissão?", peso: 2 },
-    { texto: "Você já sentiu que ele controla ou decide como você deve gastar seu dinheiro ou recursos financeiros?", peso: 1 },
-    { texto: "Ele já deixou de contribuir financeiramente com a casa ou com os filhos, mesmo tendo condições para isso?", peso: 1 },
-    { texto: "Alguma vez ele fez você assinar documentos ou contratos contra sua vontade?", peso: 2 }
-  ];
-
+  
   const [indiceAtual, setIndiceAtual] = useState(0);
   const [respostas, setRespostas] = useState([]);
   const [finalizado, setFinalizado] = useState(false);
